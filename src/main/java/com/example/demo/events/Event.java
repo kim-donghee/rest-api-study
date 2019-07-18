fbe91b2d40ccf25b3881aacd6b2d1ff48f63a953
KIM <KIM@DESKTOP-OMKLPTR>
2019-07-18 12:44:05 +0900

    POST /api/events/ return 201

--- src/main/java/com/example/demo/events/Event.java
+++ src/main/java/com/example/demo/events/Event.java
@@ -2,6 +2,12 @@ package com.example.demo.events;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -11,8 +17,10 @@ import lombok.Setter;
 
 @Getter @Setter @EqualsAndHashCode(of = { "id" })
 @Builder @AllArgsConstructor @NoArgsConstructor
+@Entity
 public class Event {
 	
+	@Id @GeneratedValue
 	private Integer id;
 	private String name;
 	private String description;
@@ -26,6 +34,7 @@ public class Event {
 	private int limitOfEnrollment;
 	private boolean offline;
     private boolean free;
+    @Enumerated(EnumType.STRING)
     private EventStatus eventStatus;
 
 }
---
+++ src/main/java/com/example/demo/events/EventController.java
@@ -0,0 +1,31 @@
+package com.example.demo.events;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+import java.net.URI;
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_UTF8_VALUE)
+public class EventController {
+	
+	private final EventRepository eventRepository;
+	
+	public EventController(EventRepository eventRepository) {
+		this.eventRepository = eventRepository;
+	}
+	
+	@PostMapping
+	public ResponseEntity createEvent(@RequestBody Event event) {
+		Event savedEvent = eventRepository.save(event);
+		URI createdUri = linkTo(EventController.class).slash(savedEvent.getId()).toUri();
+		return ResponseEntity.created(createdUri).body(event);
+	}
+	
+}
